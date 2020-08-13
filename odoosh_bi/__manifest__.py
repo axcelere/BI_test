7749f4918dfe69a511156c89730a470d27b4106d
Piyush Sharma <piyush97ps@gmail.com>
2020-08-13 17:25:55 +0530

    Module Name

--- odoosh_bi/__manifest__.py
+++ odoosh_bi/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "odoosh_bi",
+    'name': "BI Connector(SH)",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Techneith",
+    'website': "http://www.techneith.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
